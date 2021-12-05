9dd9adebc6f444c36731395fb769d2fdb0bceb75
Temperton, Zak <zbtemperton@gmail.com>
2021-12-05 21:44:42 +1300

    uses regex to choose day to run

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use regex::Regex;
+
 mod day01;
 mod day02;
 mod day03;
 mod day04;
 mod day05;
+
 fn main() {
     let mut args = std::env::args();
     if args.len() < 2 {
@@ -11,28 +14,31 @@ fn main() {
         return;
     }
     args.next(); //executable path
-    match args.next().unwrap().as_str() {
-        "day1" | "day01" | "1" => {
+    let r_day = Regex::new(r"(?:([\d]+))").unwrap();
+    let day = args.next().unwrap();
+    let captures = r_day.captures(day.as_str()).unwrap();
+    match captures.get(1).unwrap().as_str().parse::<u32>().unwrap() {
+        1 => {
             println!("day01:");
             day01::part1();
             day01::part2();
         }
-        "day2" | "day02" | "2" => {
+        2 => {
             println!("day02:");
             day02::part1();
             day02::part2();
         }
-        "day3" | "day03" | "3" => {
+        3 => {
             println!("day03:");
             day03::part1();
             day03::part2();
         }
-        "day4" | "day04" | "4" => {
+        4 => {
             println!("day04:");
             day04::part1();
             day04::part2();
         }
-        "day5" | "day05" | "5" => {
+        5 => {
             println!("day05:");
             day05::part1();
             day05::part2();
