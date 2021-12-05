7c043b1c04af8407207fd1cdac60d8587136c2ca
Temperton, Zak <zbtemperton@gmail.com>
2021-12-05 20:43:04 +1300

    tidied up day05

--- src/day05.rs
+++ src/day05.rs
@@ -9,17 +9,13 @@ pub(crate) fn part1() {
     let reader = BufReader::new(file);
     let mut map = HashMap::new();
     for line in reader.lines().flatten() {
-        let mut split = line.split(" -> ");
-        let mut p1 = split.next().unwrap().split(',');
-        let mut p2 = split.next().unwrap().split(',');
-        let p1: (i32, i32) = (
-            p1.next().unwrap().parse().unwrap(),
-            p1.next().unwrap().parse().unwrap(),
-        );
-        let p2: (i32, i32) = (
-            p2.next().unwrap().parse().unwrap(),
-            p2.next().unwrap().parse().unwrap(),
-        );
+        let mut split = line
+            .split(" -> ")
+            .map(|s| s.split(','))
+            .flatten()
+            .map(|s| s.parse().unwrap());
+        let p1: (i32, i32) = (split.next().unwrap(), split.next().unwrap());
+        let p2: (i32, i32) = (split.next().unwrap(), split.next().unwrap());
         if p1.0 == p2.0 || p1.1 == p2.1 {
             let dx = p2.0 - p1.0;
             let dy = p2.1 - p1.1;
@@ -37,17 +33,13 @@ pub(crate) fn part2() {
     let reader = BufReader::new(file);
     let mut map = HashMap::new();
     for line in reader.lines().flatten() {
-        let mut split = line.split(" -> ");
-        let mut p1 = split.next().unwrap().split(',');
-        let mut p2 = split.next().unwrap().split(',');
-        let p1: (i32, i32) = (
-            p1.next().unwrap().parse().unwrap(),
-            p1.next().unwrap().parse().unwrap(),
-        );
-        let p2: (i32, i32) = (
-            p2.next().unwrap().parse().unwrap(),
-            p2.next().unwrap().parse().unwrap(),
-        );
+        let mut split = line
+            .split(" -> ")
+            .map(|s| s.split(','))
+            .flatten()
+            .map(|s| s.parse().unwrap());
+        let p1: (i32, i32) = (split.next().unwrap(), split.next().unwrap());
+        let p2: (i32, i32) = (split.next().unwrap(), split.next().unwrap());
         let dx = p2.0 - p1.0;
         let dy = p2.1 - p1.1;
         for i in 0..=dx.abs().max(dy.abs()) {
