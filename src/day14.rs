1715d75f5a554ea1caa4b76c12ba77a7da91898c
Temperton, Zak <zbtemperton@gmail.com>
2021-12-15 11:30:34 +1300

    generalised day 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,72 +1,34 @@
 use std::{collections::HashMap, fs::read_to_string};
 
 pub(crate) fn part1() {
-    let text = read_to_string("res/day14.txt").unwrap();
-    let mut lines = text.lines();
-    let template = lines.next().unwrap().as_bytes().to_vec();
-    lines.next().unwrap();
-    let mut rules = HashMap::new();
-    for line in lines {
-        let mut split = line.split(" -> ");
-        rules.insert(
-            split.next().unwrap().as_bytes(),
-            split.next().unwrap().as_bytes()[0],
-        );
-    }
-    let mut rule_result = HashMap::new();
-    //loop once to initialise
-    for (&k, &r) in rules.iter() {
-        let mut count = [0; 26];
-        count[(r - b'A') as usize] += 1;
-        rule_result.insert(k, count);
-    }
-    //loops 9 times as we looped once for ititialising for total of 10 loops
-    for _ in 0..9 {
-        let mut new_results = HashMap::new();
-        for (&k, &r) in rules.iter() {
-            let mut count = *rule_result.get(&[k[0], r].as_ref()).unwrap();
-            for (i, c) in rule_result
-                .get(&[r, k[1]].as_ref())
-                .unwrap()
-                .iter()
-                .enumerate()
-            {
-                count[i] += c;
-            }
-            count[(r - b'A') as usize] += 1;
-            new_results.insert(k, count);
-        }
-        rule_result = new_results;
-    }
-    let mut counts = [0_u64; 26];
-    for i in 0..template.len() - 1 {
-        counts[(template[i] - b'A') as usize] += 1;
-        for (i, c) in rule_result
-            .get(&[template[i], template[i + 1]].as_ref())
-            .unwrap()
-            .iter()
-            .enumerate()
-        {
-            counts[i] += c;
-        }
-    }
-    counts[(*template.last().unwrap() - b'A') as usize] += 1;
-    let (max, min) = counts.iter().fold((u64::MIN, u64::MAX), |(max, min), &c| {
-        if c != 0 && c < min {
-            (max, c)
-        } else if c > max {
-            (c, min)
-        } else {
-            (max, min)
-        }
-    });
-    println!("part2: {}", max - min)
+    solve(10);
 }
 
 pub(crate) fn part2() {
+    solve(40);
+}
+
+fn solve(loops: usize) {
     let text = read_to_string("res/day14.txt").unwrap();
     let mut lines = text.lines();
     let template = lines.next().unwrap().as_bytes().to_vec();
+    if loops == 0 {
+        let mut counts = [0_u64; 26];
+        for c in template {
+            counts[(c - b'A') as usize] += 1;
+        }
+        let (max, min) = counts.iter().fold((u64::MIN, u64::MAX), |(max, min), &c| {
+            if c != 0 && c < min {
+                (max, c)
+            } else if c > max {
+                (c, min)
+            } else {
+                (max, min)
+            }
+        });
+        println!("part2: {}", max - min);
+        return;
+    }
     lines.next().unwrap();
     let mut rules = HashMap::new();
     for line in lines {
@@ -77,14 +39,12 @@ pub(crate) fn part2() {
         );
     }
     let mut rule_result = HashMap::new();
-    //loop once to initialise
     for (&k, &r) in rules.iter() {
         let mut count = [0; 26];
         count[(r - b'A') as usize] += 1;
         rule_result.insert(k, count);
     }
-    //loops 39 times as we looped once for ititialising for a total pof 40 loops
-    for _ in 0..39 {
+    for _ in 0..loops - 1 {
         let mut new_results = HashMap::new();
         for (&k, &r) in rules.iter() {
             let mut count = *rule_result.get(&[k[0], r].as_ref()).unwrap();
