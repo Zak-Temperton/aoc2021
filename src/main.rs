1342dcdc9ca9f0d0d252323b1487f7cd48ffb5a0
Temperton, Zak <zbtemperton@gmail.com>
2021-12-20 20:11:10 +1300

    cleaning moving clutter into mod file

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ extern crate test;
 mod solutions;
 use solutions::*;
 
-use std::fs::read_to_string;
-
 use regex::Regex;
 
 fn main() {
@@ -22,125 +20,26 @@ fn main() {
     let day = args.next().unwrap();
     let captures = r_day.captures(day.as_str()).unwrap();
     match captures.get(1).unwrap().as_str().parse::<u32>().unwrap() {
-        1 => {
-            println!("day01:");
-            let text = read_to_string("res/day01.txt").unwrap();
-            day01::part1(&text);
-            day01::part2(&text);
-        }
-        2 => {
-            println!("day02:");
-            let text = read_to_string("res/day02.txt").unwrap();
-            day02::part1(&text);
-            day02::part2(&text);
-        }
-        3 => {
-            println!("day03:");
-            let text = read_to_string("res/day03.txt").unwrap();
-            day03::part1(&text);
-            day03::part2(&text);
-        }
-        4 => {
-            println!("day04:");
-            let text = read_to_string("res/day04.txt").unwrap();
-            day04::part1(&text);
-            day04::part2(&text);
-        }
-        5 => {
-            println!("day05:");
-            let text = read_to_string("res/day05.txt").unwrap();
-            day05::part1(&text);
-            day05::part2(&text);
-        }
-        6 => {
-            println!("day06:");
-            let text = read_to_string("res/day06.txt").unwrap();
-            day06::part1(&text);
-            day06::part2(&text);
-        }
-        7 => {
-            println!("day07:");
-            let text = read_to_string("res/day07.txt").unwrap();
-            day07::part1(&text);
-            day07::part2(&text);
-        }
-        8 => {
-            println!("day08:");
-            let text = read_to_string("res/day08.txt").unwrap();
-            day08::part1(&text);
-            day08::part2(&text);
-        }
-        9 => {
-            println!("day09:");
-            let text = read_to_string("res/day09.txt").unwrap();
-            day09::part1(&text);
-            day09::part2(&text);
-        }
-        10 => {
-            println!("day10:");
-            let text = read_to_string("res/day10.txt").unwrap();
-            day10::part1(&text);
-            day10::part2(&text);
-        }
-        11 => {
-            println!("day11:");
-            let text = read_to_string("res/day11.txt").unwrap();
-            day11::part1(&text);
-            day11::part2(&text);
-        }
-        12 => {
-            println!("day12:");
-            let text = read_to_string("res/day12.txt").unwrap();
-            day12::part1(&text);
-            day12::part2(&text);
-        }
-        13 => {
-            println!("day13:");
-            let text = read_to_string("res/day13.txt").unwrap();
-            day13::part1(&text);
-            day13::part2(&text);
-        }
-        14 => {
-            println!("day14:");
-            let text = read_to_string("res/day14.txt").unwrap();
-            day14::part1(&text);
-            day14::part2(&text);
-        }
-        15 => {
-            println!("day15:");
-            let text = read_to_string("res/day15.txt").unwrap();
-            day15::part1(&text);
-            day15::part2(&text);
-        }
-        16 => {
-            println!("day16:");
-            let text = read_to_string("res/day16.txt").unwrap();
-            day16::part1(&text);
-            day16::part2(&text);
-        }
-        17 => {
-            println!("day17:");
-            let text = read_to_string("res/day17.txt").unwrap();
-            day17::part1(&text);
-            day17::part2(&text);
-        }
-        18 => {
-            println!("day18:");
-            let text = read_to_string("res/day18.txt").unwrap();
-            day18::part1(&text);
-            day18::part2(&text);
-        }
-        19 => {
-            println!("day19:");
-            let text = read_to_string("res/day19.txt").unwrap();
-            day19::part1_and_part2(&text);
-        }
-        20 => {
-            println!("day20:");
-            let text = read_to_string("res/day20.txt").unwrap();
-            day20::part1(&text);
-            day20::part2(&text);
-        }
+        1 => day01(),
+        2 => day02(),
+        3 => day03(),
+        4 => day04(),
+        5 => day05(),
+        6 => day06(),
+        7 => day07(),
+        8 => day08(),
+        9 => day09(),
+        10 => day10(),
+        11 => day11(),
+        12 => day12(),
+        13 => day13(),
+        14 => day14(),
+        15 => day15(),
+        16 => day16(),
+        17 => day17(),
+        18 => day18(),
+        19 => day19(),
+        20 => day20(),
         _ => println!("Choose a valid day"),
     }
 }
