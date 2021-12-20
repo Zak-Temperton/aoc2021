944dd32cce9c98e482289257f3032e7d5daeb5cc
Temperton, Zak <zbtemperton@gmail.com>
2021-12-20 21:16:57 +1300

    extracted solution to fuctions

--- src/solutions/day20.rs
+++ src/solutions/day20.rs
@@ -1,39 +1,7 @@
 pub fn part1(text: &str) {
-    let mut lines = text.lines();
-    let enhancement = lines
-        .next()
-        .unwrap()
-        .chars()
-        .map(|c| c == '#')
-        .collect::<Vec<_>>();
-    lines.next();
-    let mut image = create_image(lines);
-    for i in 0..2 {
-        let mut new_image =
-            vec![
-                vec![enhancement[if i & 1 == 0 { 0 } else { 0b111111111 }]; image[0].len() + 2];
-                image.len() + 2
-            ];
-        for x in 0..image[0].len() - 2 {
-            for y in 0..image.len() - 2 {
-                let mut index = 0;
-                for i in 0..3 {
-                    for j in 0..3 {
-                        index <<= 1;
-                        if image[y + i][x + j] {
-                            index |= 1;
-                        }
-                    }
-                }
-                new_image[y + 2][x + 2] = enhancement[index];
-            }
-        }
-        image = new_image;
-    }
-
     println!(
         "part1: {}",
-        image
+        enhance(text, 2)
             .drain(..)
             .flatten()
             .fold(0, |a, p| if p { a + 1 } else { a })
@@ -41,16 +9,18 @@ pub fn part1(text: &str) {
 }
 
 pub fn part2(text: &str) {
-    let mut lines = text.lines();
-    let enhancement = lines
-        .next()
-        .unwrap()
-        .chars()
-        .map(|c| c == '#')
-        .collect::<Vec<_>>();
-    lines.next();
-    let mut image = create_image(lines);
-    for i in 0..50 {
+    println!(
+        "part2: {}",
+        enhance(text, 50)
+            .drain(..)
+            .flatten()
+            .fold(0, |a, p| if p { a + 1 } else { a })
+    )
+}
+
+fn enhance(text: &str, turns: usize) -> Vec<Vec<bool>> {
+    let (mut image, enhancement) = create_image(text);
+    for i in 0..turns {
         let mut new_image =
             vec![
                 vec![enhancement[if i & 1 == 0 { 0 } else { 0b111111111 }]; image[0].len() + 2];
@@ -72,38 +42,29 @@ pub fn part2(text: &str) {
         }
         image = new_image;
     }
-
-    println!(
-        "part2: {}",
-        image
-            .drain(..)
-            .flatten()
-            .fold(0, |a, p| if p { a + 1 } else { a })
-    )
+    image
 }
 
-fn create_image(mut lines: std::str::Lines) -> Vec<Vec<bool>> {
-    let mut image = Vec::new();
-    let mut first = vec![false; 3];
-    first.append(&mut lines.next().unwrap().chars().map(|c| c == '#').collect());
-    first.append(&mut vec![false; 3]);
+fn as_bools(line: &str) -> Vec<bool> {
+    line.chars().map(|c| c == '#').collect()
+}
+
+fn create_image(text: &str) -> (Vec<Vec<bool>>, Vec<bool>) {
+    let mut lines = text.lines();
+    let enhancement = as_bools(lines.next().unwrap());
+    lines.next();
+    let first = prepend_append(lines.next().unwrap());
     let width = first.len();
-    for _ in 0..3 {
-        image.push(vec![false; width]);
-    }
+    let mut image = vec![vec![false; width]; 2];
     image.push(first);
-    image.append(
-        &mut lines
-            .map(|line| {
-                let mut v = vec![false; 3];
-                v.append(&mut line.chars().map(|c| c == '#').collect());
-                v.append(&mut vec![false; 3]);
-                v
-            })
-            .collect(),
-    );
-    for _ in 0..3 {
-        image.push(vec![false; width]);
-    }
-    image
+    image.append(&mut lines.map(prepend_append).collect());
+    image.append(&mut vec![vec![false; width]; 2]);
+    (image, enhancement)
+}
+
+fn prepend_append(line: &str) -> Vec<bool> {
+    let mut out = vec![false; 2];
+    out.append(&mut as_bools(line));
+    out.append(&mut vec![false; 2]);
+    out
 }
